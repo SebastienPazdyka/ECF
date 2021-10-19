3cb4ff9c1ef751f9b1bf4e3e90feb688d82e1f6a
SebastienPazdyka <77808685+SebastienPazdyka@users.noreply.github.com>
2021-10-19 19:36:30 +0200

    Add files via upload

--- config.php
+++ config.php
@@ -1,18 +1,10 @@
 <?php
+ {
 
-if (getenv('JAWSDB_URL') !== false) {
-$dbparts = parse_url(getenv('JAWSDB_URL'));
-
-$hostname = $dbparts['x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'];
-$username = $dbparts['r332ceqy8890pekr'];
-$password = $dbparts['c5n2l42x4ll5phvi'];
-$database = ltrim($dbparts['anar4g3bxb1ant68'],'/');
-} else {
-
-$host = "localhost";
-$user = "root";
-$pass = "Y28IE09ihjdved";
-$dbname = "mediatheque";
+$host = "x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$user = "r332ceqy8890pekr";
+$pass = "c5n2l42x4ll5phvi";
+$dbname = "anar4g3bxb1ant68";
 $conn = new mysqli($host , $user, $pass, $dbname);
 mysqli_query($conn , "SET character_set_result=utf8");
 if($conn->connect_error){
