52e91cb737b6c1cae3e03508b99e91bc57a7c5ee
SebastienPazdyka <77808685+SebastienPazdyka@users.noreply.github.com>
2021-10-21 01:10:51 +0200

    Add files via upload

--- config.php
+++ config.php
@@ -1,12 +1,16 @@
 <?php
- {
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=mediatheque;charset=utf8', 'root', 'Y28IE09ihjdved');
+} catch (Exception $e) {
+    die('Erreur' . $e->getMessage());
+}
 
-$host = "x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-$user = "r332ceqy8890pekr";
-$pass = "c5n2l42x4ll5phvi";
-$dbname = "anar4g3bxb1ant68";
+$host = "localhost";
+$user = "root";
+$pass = "Y28IE09ihjdved";
+$dbname = "mediatheque";
 $conn = new mysqli($host , $user, $pass, $dbname);
 mysqli_query($conn , "SET character_set_result=utf8");
 if($conn->connect_error){
-    die("Database Error : " . $conn->connect_error);}
+    die("Database Error : " . $conn->connect_error);
 }
